ba9cdc788c5dde9471859d4da8e5936d47160312
Abubazar <sediqdude360@gmail.com>
2025-11-05 06:59:51 +0100

    edited the login page a bit

--- logscript.js
+++ logscript.js
@@ -5,7 +5,9 @@ var password
 const title = document.getElementById("title")
 const  button = document.getElementById("continue")
 const  switchBtn = document.getElementById("switch")
+const  warn = document.getElementById("warning")
 
+warn.style.display = "none"
 
 function switcher(){
     if (state=="login"){
@@ -13,11 +15,13 @@ function switcher(){
         title.textContent = "Create A New Account"
         button.textContent = "Create Account"
         switchBtn.textContent = "Login To Account"
+        warn.style.display = "block"
     }
     else{
         state="login"
         title.textContent = "Log Into Your Account"
         button.textContent = "Log In"
         switchBtn.textContent = "Create An Account"
+        warn.style.display = "none"
     }
 }
