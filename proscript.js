c14f4e9e7827d1917c7d03030553ea3f2eab3674
Abubazar <sediqdude360@gmail.com>
2025-11-05 09:28:30 +0100

    finally getting somewhere with the js. i might have to create a test html to test how to use firebase with this thing

--- proscript.js
+++ proscript.js
@@ -1,8 +1,17 @@
 const miniDialog = document.getElementById("miniDialog")
+const verText = document.getElementById("verText")
+const VerAnswer = document.getElementById("verAnswer")
+
+const titleLink = document.getElementById("titleLink")
+const urlLink = document.getElementById("urlLink")
 
 
 function addLink(){
     miniDialog.style.visibility = "visible"
+    // verText.textContent=""
+    verAnswer.value=""
+    titleLink.value=""
+    urlLink.value=""
 }
 
 function cancel(){
