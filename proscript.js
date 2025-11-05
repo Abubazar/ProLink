a60a3bf44440e7399fc2e7f17c18d48e7fff8c95
Abubazar <sediqdude360@gmail.com>
2025-11-05 07:59:32 +0100

    started the js code for the homepage. will soon get into the firebase

---
+++ proscript.js
@@ -0,0 +1,14 @@
+const miniDialog = document.getElementById("miniDialog")
+
+
+function addLink(){
+    miniDialog.style.visibility = "visible"
+}
+
+function cancel(){
+    miniDialog.style.visibility = "hidden"
+}
+
+function accept(){
+    miniDialog.style.visibility = "hidden"
+}
